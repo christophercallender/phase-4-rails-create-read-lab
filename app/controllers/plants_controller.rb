1feb52884a4b7161ada4cdcc3339a582a8d20254
Christopher Callender <christophercallender0000@gmail.com>
2022-08-16 20:06:25 -0400

    Completed assignment

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,11 @@
+class PlantsController < ApplicationController
+  def index
+    render json: Plant.all
+  end
+  def show
+    render json: Plant.find(params[:id])
+  end
+  def create
+    render json: Plant.create(params.permit(:name, :image, :price)), status: 201
+  end
+end
